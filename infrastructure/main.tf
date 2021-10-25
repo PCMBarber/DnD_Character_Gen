970039910efa9a6500dbb80dae9214a4d24204ac
Piers Barber <Kochab.96@gmail.com>
2021-10-25 19:04:00 +0100

    took out user_data for node groups

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -74,14 +74,12 @@ module "eks" {
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
       instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
